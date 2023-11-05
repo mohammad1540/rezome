2a9c9cf50b2809397941a8a96a7557b31df49d5c
mohammad <mohammad.babaloee@gmail.com~>
2023-11-06 01:10:24 +0330

    v1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let menu = document.getElementById("menu");
+let menu1 = document.getElementById("menu1");
+let menu2 = document.getElementById("menu2");
+menu.addEventListener("click", function () {
+  menu1.style.opacity = 1;
+
+});
+
