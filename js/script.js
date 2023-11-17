b5da9bb562fb31be431f1e5c8dad6d9ffa90f8b9
mohammad1540 <143171212+mohammad1540@users.noreply.github.com>
2023-11-17 17:04:26 +0330

    full rezome

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 let menu = document.getElementById("menu");
 let menu1 = document.getElementById("menu1");
 let menu2 = document.getElementById("menu2");
+let close = document.getElementById("mohammadclose");
 menu.addEventListener("click", function () {
-  menu1.style.opacity = 1;
-
+  menu1.classList.add("active");
+  menu1.classList.add("mohammad");
+  menu.classList.add("noactive");
+  menu1.classList.remove("mohammadclose");
 });
+close.addEventListener("click", function () {
+  menu1.classList.add("mohammadclose");
+  menu1.classList.remove("mohammad");
 
+  menu.classList.remove("noactive");
+  setTimeout(() => {
+    menu1.classList.remove("active");
+  }, 300);
+});
